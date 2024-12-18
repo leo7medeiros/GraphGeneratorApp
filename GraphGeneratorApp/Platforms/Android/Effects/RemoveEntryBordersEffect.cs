7c30bedcd58ba15760681ff4e958b173241557c8
Leonardo <79287199+leo7medeiros@users.noreply.github.com>
2024-12-18 00:48:49 -0300

    Subindo ajustes nas cores e ícones padrão da aplicação

---
+++ GraphGeneratorApp/Platforms/Android/Effects/RemoveEntryBordersEffect.cs
@@ -0,0 +1,27 @@
+﻿using Android.Widget;
+using GraphGeneratorApp.Dependências.Effects;
+using Droid = Android;
+
+[assembly: ResolutionGroupName("MauiAppProject.Dependencias.Effects")]
+[assembly: ExportEffect(typeof(RemoveEntryBordersEffect), nameof(RemoveEntryBordersEffect))]
+namespace GraphGeneratorApp.Platforms.Android.Effects
+{
+    public class RemoveEntryBordersEffect : PlatformEffect<Entry, EditText>
+    {
+        protected override void OnAttached()
+        {
+            if (Control is EditText editText)
+            {
+                editText.SetBackgroundColor(Droid.Graphics.Color.Transparent);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            if (Control is EditText editText)
+            {
+                editText.SetBackgroundColor(Droid.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
